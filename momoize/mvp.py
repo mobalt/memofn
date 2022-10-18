f9e693d54c339c6ddc216eb0fd769144988b2c37
Moises Baltazar <me@moisesb.com>
2022-10-18 18:09:25 -0400

    Add expiration capabilities

--- momoize/mvp.py
+++ momoize/mvp.py
@@ -1,5 +1,6 @@
 import datetime
 import functools
+import types
 
 from momoize.utils.hashable import make_hashable
 
@@ -34,9 +35,23 @@ def namespace(fn):
 global_cache = {}
 
 
-def memofn(fn=None, expire_in_days=None, ns=""):
+def always_false(*args, **kwargs):
+    return False
+
+
+def memofn(
+    fn=None, expire_in_days=None, ns="", unless_fn=None, ignore_first_n_args=None
+):
     if fn is None:
-        return functools.partial(memofn, expire_in_days=expire_in_days, ns=ns)
+        return functools.partial(
+            memofn, expire_in_days=expire_in_days, ns=ns, unless_fn=unless_fn
+        )
+
+    is_method = isinstance(fn, types.MethodType)
+    if ignore_first_n_args is None:
+        ignore_first_n_args = 1 if is_method else 0
+    if unless_fn is None:
+        unless_fn = always_false
 
     global global_cache
     if not ns:
@@ -47,12 +62,20 @@ def memofn(fn=None, expire_in_days=None, ns=""):
 
     @functools.wraps(fn)
     def wrapper(*args, **kwargs):
-        key = make_hashable((args, kwargs))
-
+        key = make_hashable((args[ignore_first_n_args:], kwargs))
         if key in local_cache:
             val = local_cache[key]
-            val.bump()
-            return val.value
+            if unless_fn(*args, **kwargs) or (
+                expire_in_days is not None and (val.created_days_ago > expire_in_days)
+            ):
+                del local_cache[key]
+                # calc new
+                val = fn(*args, **kwargs)
+                local_cache[key] = MemoValue(val)
+                return val
+            else:
+                val.bump()
+                return val.value
         else:
             val = fn(*args, **kwargs)
             local_cache[key] = MemoValue(val)
