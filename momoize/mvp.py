52cfedb31465de179e641ee256d719597d5f0162
Moises Baltazar <me@moisesb.com>
2022-11-01 11:59:05 -0400

    Add comments

--- momoize/mvp.py
+++ momoize/mvp.py
@@ -42,14 +42,18 @@ def always_false(*args, **kwargs):
 def memofn(
     fn=None, expire_in_days=None, ns="", unless_fn=None, ignore_first_n_args=None
 ):
+    # If fn is not provided, then curry the function
     if fn is None:
         return functools.partial(
             memofn, expire_in_days=expire_in_days, ns=ns, unless_fn=unless_fn
         )
 
+    # If first arg is self, then ignore it
     is_method = isinstance(fn, types.MethodType)
     if ignore_first_n_args is None:
         ignore_first_n_args = 1 if is_method else 0
+
+    # If no unless_fn is provided, then use a function that always returns False
     if unless_fn is None:
         unless_fn = always_false
 
@@ -62,23 +66,23 @@ def memofn(
 
     @functools.wraps(fn)
     def wrapper(*args, **kwargs):
+        is_method = isinstance(wrapper, types.MethodType)
         key = make_hashable((args[ignore_first_n_args:], kwargs))
+
         if key in local_cache:
-            val = local_cache[key]
-            if unless_fn(*args, **kwargs) or (
-                expire_in_days is not None and (val.created_days_ago > expire_in_days)
+            memo_value = local_cache[key]
+            # Should we use cached value? Or recalculate a new one?
+            if not unless_fn(*args, **kwargs) or (
+                expire_in_days is None or memo_value.created_days_ago <= expire_in_days
             ):
-                del local_cache[key]
-                # calc new
-                val = fn(*args, **kwargs)
-                local_cache[key] = MemoValue(val)
-                return val
-            else:
-                val.bump()
-                return val.value
-        else:
-            val = fn(*args, **kwargs)
-            local_cache[key] = MemoValue(val)
-            return val
+                memo_value.bump()
+                return memo_value.value
+
+            # Delete old value
+            del local_cache[key]
+        # Calculate new value
+        value = fn(*args, **kwargs)
+        local_cache[key] = MemoValue(value)
+        return value
 
     return wrapper
